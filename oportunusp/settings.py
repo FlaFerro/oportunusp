ac94606a90830283bc0261dc74056b5f2be09e74
Flavio Silva Ferro <flavioferro@usp.br>
2024-11-28 14:55:33 -0300

    render v4

--- oportunusp/settings.py
+++ oportunusp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,5 +175,4 @@ import os
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-'
 
